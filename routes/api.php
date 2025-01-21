6e58b983b97e29d72674fe464603abdc387eafbd
EduardoGMora <lalogomo2000@outlook.com>
2025-01-21 12:50:26 -0600

    Add Sanctum configuration, migration for personal access tokens, and API routes for categories, recipes, and tags

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\CategoryController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('categories',        [CategoryController::class,'index']);
+Route::get('categories/{id}',   [CategoryController::class,'show']);
+
+Route::get('recipes',           [RecipeController::class,'index']);
+Route::get('recipes/{id}',      [RecipeController::class,'show']);
+
+Route::get('tags',              [TagController::class,'index']);
+Route::get('tags/{id}',         [TagController::class,'show']);
+
