55b516a4d4e4c1bbd526ffa0f64b5b6c9fcc41aa
EduardoGMora <lalogomo2000@outlook.com>
2025-01-21 13:44:02 -0600

    Refactor recipe routes to use apiResource for cleaner route definitions

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,7 @@ use App\Http\Controllers\Api\CategoryController;
 Route::get('categories',        [CategoryController::class,'index']);
 Route::get('categories/{id}',   [CategoryController::class,'show']);
 
-Route::get('recipes',           [RecipeController::class,'index']);
-Route::get('recipes/{id}',      [RecipeController::class,'show']);
+Route::apiResource('recipes',   RecipeController::class);
 
 Route::get('tags',              [TagController::class,'index']);
 Route::get('tags/{id}',         [TagController::class,'show']);
