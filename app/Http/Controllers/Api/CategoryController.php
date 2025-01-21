62db0c14c6ff7ed6f89c95321426ee8f0536b0b4
EduardoGMora <lalogomo2000@outlook.com>
2025-01-21 14:07:12 -0600

    Add CategoryResource and CategoryCollection for API responses, make it simple and easier to read

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,14 +6,20 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// add resource
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\CategoryCollection;
+
+
 class CategoryController extends Controller
 {
     //
     public function index(){
-        return Category::all();
+        return new CategoryCollection(Category::all());
     }
 
     public function show($id){
-        return Category::find($id)->load('recipes');
+        $category = Category::find($id)->load('recipes');
+        return new CategoryResource($category);
     }
 }
