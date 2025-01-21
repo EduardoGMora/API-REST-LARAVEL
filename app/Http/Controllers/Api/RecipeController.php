c131b631a37ee8f18e1aa51a9133e26407ef29af
EduardoGMora <lalogomo2000@outlook.com>
2025-01-21 13:43:53 -0600

    Add CRUD methods to RecipeController

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -8,12 +8,25 @@ use App\Http\Controllers\Controller;
 
 class RecipeController extends Controller
 {
-    //
+    // show all recipes
     public function index(){
         return Recipe::with('category','tags', 'user')->get();
     }
 
+    // store new recipe
+    public function strore(Request $request){
+    }
+
+    // show 1 recipe
     public function show($id){
         return Recipe::find($id)->load('category','tags', 'user');
+    }
+
+    // update recipe
+    public function update(Request $request, $id){
+    }
+
+    // delete recipe
+    public function destroy($id){
     }
 }
