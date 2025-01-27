1c4624f7453891b0a4123fd9e74b45cc57991dfc
EduardoGMora <lalogomo2000@outlook.com>
2025-01-27 11:25:39 -0600

    Refactor RecipeController to use form requests for validation and implement pagination for recipe listings

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -5,53 +5,49 @@ namespace App\Http\Controllers\Api;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Symfony\Component\HttpFoundation\Response;
-
-// add resources
 use App\Http\Resources\RecipeResource;
+use App\Http\Requests\StoreRecipeRequest;
+use App\Http\Requests\UpdateRecipeRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class RecipeController extends Controller
 {
     // show all recipes
     public function index(){
-        $recipes = Recipe::with('category','tags', 'user')->get();
+        $recipes = Recipe::with('category','tags', 'user')->paginate(10);
         return RecipeResource::collection($recipes);
     }
 
     // store new recipe
-    public function store(Request $request){
-
+    public function store(StoreRecipeRequest $request){
         $recipe = Recipe::create($request->all());
 
-        if ($tags = json_decode($request->tags)) {
-            $recipe->tags()->attach($tags);
-        }
-
+        $recipe->tags()->attach(json_decode($request->tags));
+        $recipe->load('category', 'tags', 'user');
         return response()->json(new RecipeResource($recipe), Response::HTTP_CREATED); // HTTP_CREATED = 201
     }
 
     // show 1 recipe
-    public function show($id){
-        $recipe = Recipe::find($id)->load('category','tags', 'user');
+    public function show(Recipe $recipe){
+        $recipe = $recipe->load('category','tags', 'user');
         return new RecipeResource($recipe);
     }
 
     // update recipe
-    public function update(Request $request, $id){
-        $recipe = Recipe::find($id);
+    public function update(UpdateRecipeRequest $request, Recipe $recipe){
         $recipe->update($request->all());
 
         if ($tags = json_decode($request->tags)) {
             $recipe->tags()->sync($tags);
         }
-
+        $recipe->load('category', 'tags', 'user');
         return response()->json( new RecipeResource($recipe), Response::HTTP_ACCEPTED ); // HTTP_ACCEPTED = 202
     }
 
     // delete recipe
-    public function destroy($id){
-        $recipe = Recipe::find($id);
+    public function destroy(Recipe $recipe){
         $recipe->delete();
+
         return response()->json(null, Response::HTTP_NO_CONTENT); // HTTP_NO_CONTENT = 204
     }
 }
